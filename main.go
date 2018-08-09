f289a997ee03c5719a1c308b114940b5490218cc
Milan Aleksic <milanaleksic@gmail.com>
2018-08-09 11:25:42 +0200

    walking skeleton part 1: make sure you can do a simple info call

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+		"github.com/milanaleksic/amtgo/amt"
+	"flag"
+)
+
+
+func main() {
+	username := flag.String("username", "", "Username for the AMT interface")
+	password := flag.String("password", "", "Password for the AMT interface")
+
+	options := amt.Optionset{
+		SwSkipcertchk: 1,
+		SwUseTLS: 0,
+		Username: *username,
+		Password: *password,
+		Port: 16888,
+	}
+	amt.CliCommand(amt.CmdInfo, []string { "localhost" }, options)
+}
